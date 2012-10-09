e4955d76aff93a260fd29d83d83b558d24e8b897
asparkman <alex.sparkman@iinteractive.com>
2012-10-09 16:01:55 -0500

    Rearranged test cases into a more logical format.
    Added a main entry point class for creating the test suite that this project intends to create.

---
+++ IInteractive.WebTest/WebSiteTestSuiteGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IInteractive.WebTest
+{
+    /// <summary>
+    /// The purpose of this class is to serve as an entry point into this 
+    /// application for testing processes.
+    /// </summary>
+    public class WebSiteTestSuiteGenerator
+    {
+        public List<WebPage> Seeds { get; set; }
+        public List<Browser> Browser { get; set; }
+
+        public WebSiteTestSuiteGenerator(List<WebPage> Seeds, List<Browser> Browser)
+        {
+            this.Seeds = Seeds;
+            this.Browser = Browser;
+        }
+    }
+}
