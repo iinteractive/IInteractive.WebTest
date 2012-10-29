f4718e9ff858ffed1c1d62dd91de5e07fc362646
asparkman <alex.sparkman@iinteractive.com>
2012-10-29 10:09:45 -0500

    Refs LOD-76.
    The HtmlValidationResult was added to decouple the control logic from the data model of the validation service result.  The HtmlValidator class was added to serve as the new HTMLValidate class.  The HTMLValidate class will eventually be removed once the HtmlValidationResult and HtmlValidator classes have been written.

---
+++ IInteractive.MarkupValidator/Controllers/HtmlValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IInteractive.MarkupValidator.Controllers
+{
+    /// <summary>
+    /// The purpose of this class is to hold the control logic for making 
+    /// requests to the W3C validation service.
+    /// </summary>
+    public class HtmlValidator
+    {
+    }
+}
---
+++ IInteractive.MarkupValidator/Models/HtmlValidationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IInteractive.MarkupValidator.Models
+{
+
+    /// <summary>
+    /// This class serves as a POCO for holding the information returned from 
+    /// the HTML Validation service.
+    /// </summary>
+    public class HtmlValidationResult
+    {
+    }
+}
